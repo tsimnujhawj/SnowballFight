260a7bbb2f34a1a322d6cf1e7645eab559f40e01
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-02-27 17:44:34 -0600

    clamped paddle

--- SnowballFightWithTheNasties/Assets/Scripts/Paddle.cs
+++ SnowballFightWithTheNasties/Assets/Scripts/Paddle.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,7 @@ public class Paddle : MonoBehaviour
     {
         float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
         transform.position = paddlePos;
     }
 }
