e79ffdc72fc1265b7088b8ece9fd27ebf9f9f3a5
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-02-28 14:31:58 -0600

    added game speed

---
+++ SnowballFightWithTheNasties/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+
+    [Range(0.1f, 10.0f)] [SerializeField] float gameSpeed = 0.70f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
