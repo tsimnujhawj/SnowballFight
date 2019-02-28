96f84de401fb375c5c2d976331b41efdc4b7870e
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-02-28 12:02:17 -0600

    added sfx to ball collusion

--- SnowballFightWithTheNasties/Assets/Scripts/Ball.cs
+++ SnowballFightWithTheNasties/Assets/Scripts/Ball.cs
@@ -9,16 +9,21 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
 
 
     // states
     Vector2 paddleToBallVector;
     bool hasStarted = false;
 
+    // cached components
+    AudioSource myAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -49,7 +54,8 @@ public class Ball : MonoBehaviour
     {
         if (hasStarted)
         {
-            GetComponent<AudioSource>().Play();
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
         }
     }
 
