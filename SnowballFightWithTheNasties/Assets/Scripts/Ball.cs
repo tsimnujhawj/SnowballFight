33431d67954d5d060dd0bc12e4ab6395e573fcf0
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-02-27 18:15:23 -0600

    bond ball to paddle

--- SnowballFightWithTheNasties/Assets/Scripts/Ball.cs
+++ SnowballFightWithTheNasties/Assets/Scripts/Ball.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+
+    // config paramter
+    [SerializeField] Paddle paddle1;
+
+    // state
+    Vector2 paddleToBallVector;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        paddleToBallVector = transform.position - paddle1.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddleToBallVector + paddlePos;
     }
 }
