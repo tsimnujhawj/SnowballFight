725f1f136331a03c645ad8be7976e0c7978d0139
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-02-28 14:59:22 -0600

    added UI for score

--- SnowballFightWithTheNasties/Assets/Scripts/GameStatus.cs
+++ SnowballFightWithTheNasties/Assets/Scripts/GameStatus.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     // configurations
     [Range(0.1f, 10.0f)] [SerializeField] float gameSpeed = 0.70f;
     [SerializeField] int pointsPerBlockDestroyed = 10;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // state variables
     [SerializeField] int currentScore = 0;
+
+    private void Start()
+    {
+        
+    }
 
     void Update()
     {
